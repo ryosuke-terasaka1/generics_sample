d5367bf27e1576fba13759d376abc1ee43a291cd
ryosuke-terasaka1 <terasaka_ryosuke@outlook.jp>
2022-06-30 23:01:57 +0900

    型パラメータを試してみた できてはいない

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
-package generics_sample
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(f([]MyStr{"1", "2", "3", "4"}))
+}
+
+func f[T Integers](xs []T) []int {
+	var result []int
+	for _, x := range xs {
+		result = append(result, x.Integer())
+	}
+	return result
+}
+
+type Integers interface {
+	Integer() int
+}
+
+type MyStr string
+
+func (i MyStr) Integer() (int, error) {
+	return strconv.Atoi(string(i))
+}

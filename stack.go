5a43b7e717d157edaf844217a39b695c327c422a
ryosuke-terasaka1 <terasaka_ryosuke@outlook.jp>
2022-07-01 23:15:39 +0900

    stack作成

---
+++ stack.go
@@ -0,0 +1,18 @@
+package main
+
+type Stack[T any] []T
+
+func New[T any]() *Stack[T] {
+	v := make(Stack[T], 0)
+	return &v
+}
+
+func (s *Stack[T]) Push(x T) {
+	(*s) = append((*s), x)
+}
+
+func (s *Stack[T]) Pop() T {
+	v := (*s)[len(*s)-1]
+	(*s) = (*s)[:len(*s)-1]
+	return v
+}

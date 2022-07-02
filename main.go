c7f416bfcf02d6814bfe28809ce1c864c110cb3f
ryosuke-terasaka1 <terasaka_ryosuke@outlook.jp>
2022-07-03 01:26:15 +0900

    mainファイル作成

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"generics_sample/test_code"
+)
 
 func main() {
-	fmt.Println(f([]MyInt{1, 2, 3, 4}))
-
-	s := New[string]()
-	s.Push("hello")
-	s.Push("world")
-	fmt.Println(s.Pop()) // world
-	fmt.Println(s.Pop()) // hello
-
-	stringValue := "111"
-	intValue := 111
-	boolValue := false
-
-	fmt.Printf("sampleFuncGenerics1: %#v\n", sampleFuncGenerics1(stringValue))
-	fmt.Printf("sampleFuncGenerics1: %#v\n", sampleFuncGenerics1(intValue))
-	fmt.Printf("sampleFuncGenerics1: %#v\n", sampleFuncGenerics1(boolValue))
+	int_slice := []int{1, 2, 3, 4, 5}
+	int_av := test_code.Average(int_slice)
+	fmt.Printf("int_cal=%v\n", int_av)
+	float_slice := []float64{1.2, 2.3, 3.0, 4.9, 5.1}
+	float_av := test_code.Average(float_slice)
+	fmt.Printf("float_cal=%v\n", float_av)
 }

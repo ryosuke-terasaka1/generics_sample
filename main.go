0d14d2f7b63c391e117c66c81ef156a7903ad24a
ryosuke-terasaka1 <terasaka_ryosuke@outlook.jp>
2022-07-02 20:59:04 +0900

    comparable作成

--- main.go
+++ main.go
@@ -10,4 +10,12 @@ func main() {
 	s.Push("world")
 	fmt.Println(s.Pop()) // world
 	fmt.Println(s.Pop()) // hello
+
+	stringValue := "111"
+	intValue := 111
+	boolValue := false
+
+	fmt.Printf("sampleFuncGenerics1: %#v\n", sampleFuncGenerics1(stringValue))
+	fmt.Printf("sampleFuncGenerics1: %#v\n", sampleFuncGenerics1(intValue))
+	fmt.Printf("sampleFuncGenerics1: %#v\n", sampleFuncGenerics1(boolValue))
 }
